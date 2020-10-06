1866511a7793ceb4b97e2ee013e04570941af454
Nicholai Nissen <nicholainissen@gmail.com>
2020-10-06 20:10:49 +0200

    feat(ui): scaffold the bare minimum ui package
    Read the readme

---
+++ packages/ui/src/store/dummy-store.ts
@@ -0,0 +1,17 @@
+import { Store } from './store'
+
+interface Dummy extends Object {
+  count: number
+}
+
+class DummyStore extends Store<Dummy> {
+  protected data(): Dummy {
+    return {
+      count: 0
+    }
+  }
+
+  incrementCount () {
+    this.state.count++
+  }
+}
