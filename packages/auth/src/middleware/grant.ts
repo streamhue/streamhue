cd78d3ca33cb2eab2f626c34498f6a2ed45d8e9c
Nicholai Nissen <nicholainissen@gmail.com>
2020-10-12 15:41:28 +0200

    feat(auth): add minimal grant mw

---
+++ packages/auth/src/middleware/grant.ts
@@ -0,0 +1,17 @@
+import grant, { GrantOptionDefaults } from 'grant'
+
+function assignDefaults (): GrantOptionDefaults {
+  const isDev = process.env.NODE_ENV !== 'production'
+
+  return {
+    state: true,
+    protocol: isDev ? 'http' : 'https',
+    host: isDev ? 'localhost:3000' : 'auth.streamhue.com'
+  }
+}
+
+export default function () {
+  return grant.curveball({
+    defaults: assignDefaults()
+  })
+}
