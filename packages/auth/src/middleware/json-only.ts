6e8f99f433724533709a34ad98884ed9b33ceb28
Nicholai Nissen <nicholainissen@gmail.com>
2020-10-24 14:08:04 +0200

    feat(auth): enforce json responses

--- packages/auth/src/middleware/json-only.ts
+++ packages/auth/src/middleware/json-only.ts
@@ -1,11 +1,20 @@
 import { Middleware } from '@curveball/core/dist'
 
+const isDev = process.env.NODE_ENV !== 'production'
+
 export function jsonOnly (): Middleware {
   return async (ctx, next) => {
     ctx.response.headers.set('Content-Type', 'application/json')
+
     await next()
     if (!ctx.response.is('json') || typeof ctx.response.body !== 'object') {
       throw new Error('Response is expected to be JSON')
     }
+
+    if (isDev) ctx.response.body = prettify(ctx.response.body)
   }
+}
+
+function prettify <T extends object> (obj: T): String {
+  return JSON.stringify(obj, null, 2)
 }
