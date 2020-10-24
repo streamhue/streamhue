8ea66253da74785f9ba2cb2b8c6f95a6b052f482
Nicholai Nissen <nicholainissen@gmail.com>
2020-10-24 14:07:24 +0200

    fix(auth): add missing changes
    remove dotenv from index and add modular callback routes

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,10 +1,8 @@
+
 import { app } from './app'
 import process from 'process'
-import * as dotenv from 'dotenv'
 import { database } from '@streamhue/common'
 import { PackageJson } from '@streamhue/common/types/package-json'
-
-dotenv.config()
 
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const pkg: PackageJson = require('../package.json')
@@ -19,6 +17,6 @@ async function start (uri: string | undefined, port: string) {
   console.log(`Listening on port ${port}`)
 }
 
-start(process.env.MONGO_URI, process.env.PORT ?? '6000').catch(
+start(process.env.MONGO_URI, process.env.PORT ?? '3000').catch(
   error => console.error(error)
 )
--- packages/auth/src/routes.ts
+++ packages/auth/src/routes.ts
@@ -2,8 +2,10 @@
 import router from '@curveball/router'
 import homeController from './home/controller'
 import loginController from './login/controller'
+import oauthConnectController from './oauth/connect/controller'
 
 export default [
   router('/', homeController),
-  router('/login', loginController)
+  router('/login', loginController),
+  router('/vendor/:type/:vendor/callback', oauthConnectController)
 ]
